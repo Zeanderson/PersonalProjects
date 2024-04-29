c760e864b43361707d9156a35ba98857ee5394ff
Zeanderson <121685062+Zeanderson@users.noreply.github.com>
2024-04-28 20:11:00 -0500

    Next JS Init code (https://www.youtube.com/watch?v=gfQCNuTMIGs)

---
+++ projects-webapp/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>Next TS Starter</h1>
+      </div>
+    </section>
+  )
+}
